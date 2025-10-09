989bcfb9baac369bd13a5221bc575342a46d8bc6
Terrence <terrence3v@gmail.com>
2025-10-09 17:00:21 -0400

    Servo update fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/champion/controller/ServoController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/champion/controller/ServoController.java
@@ -14,7 +14,12 @@ public class ServoController {
     public static double AXON_MINI_QUARTER_POSITION = 0.25;
     public static double AXON_MINI_THREE_QUARTER_POSITION = 0.75;
 
+    private enum ServoMode {
+        POSITION
+    }
+
     private final Servo axonMini;
+    private ServoMode servoMode = ServoMode.POSITION;
 
     public ServoController(LinearOpMode opMode) {
         axonMini = opMode.hardwareMap.get(Servo.class, AXON_MINI_NAME);
@@ -48,5 +53,68 @@ public class ServoController {
 
     public void setToThreeQuarter() {
         setPosition(AXON_MINI_THREE_QUARTER_POSITION);
+    }
+
+    public void stop() {
+        setPosition(AXON_MINI_HALF_POSITION);
+    }
+
+    public void forwardFull() {
+        setPosition(AXON_MINI_MAX_POSITION);
+    }
+
+    public void forwardHalf() {
+        setPosition(0.75);
+    }
+
+    public void forwardQuarter() {
+        setPosition(0.625);
+    }
+
+    public void forwardSlow() {
+        setPosition(0.55);
+    }
+
+    public void reverseFull() {
+        setPosition(AXON_MINI_MIN_POSITION);
+    }
+
+    public void reverseHalf() {
+        setPosition(0.25);
+    }
+
+    public void reverseQuarter() {
+        setPosition(0.375);
+    }
+
+    public void reverseSlow() {
+        setPosition(0.45);
+    }
+
+    public double getPower() {
+        // Map position back to power (-1 to 1)
+        return (getPosition() * 2.0) - 1.0;
+    }
+
+    public boolean isMovingForward() {
+        return getPosition() > AXON_MINI_HALF_POSITION;
+    }
+
+    public boolean isMovingReverse() {
+        return getPosition() < AXON_MINI_HALF_POSITION;
+    }
+
+    public boolean isStopped() {
+        return getPosition() == AXON_MINI_HALF_POSITION;
+    }
+
+    public void setPower(double power) {
+        // Map power (-1 to 1) to position (0 to 1)
+        double position = (power + 1.0) / 2.0;
+        setPosition(position);
+    }
+
+    public ServoMode getMode() {
+        return servoMode;
     }
 }
